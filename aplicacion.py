4f9267422ebbf2e2c156a123858f4a2b3bb7869b
Gaby Reyes <gabreyes2908@gmail.com>
2014-06-06 22:28:41 -0600

    Con modulo

--- aplicacion.py
+++ aplicacion.py
@@ -1,12 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, abort, session
+import compilador_sml
 
 app = Flask(__name__)
-
-
+#Pagina principal
 @app.route('/')
 def home():
     return render_template('principal.html')
 
-
+#Pagina para subir achivo, recibe el archivo
 @app.route('/upload_file',  methods=['POST'])
 def upload_file():
     if request.method == 'POST':
@@ -15,23 +16,24 @@ def upload_file():
     	lineas=''
     	for linea in f :
         	lineas+=linea
-		print"Lee linea"
-    	Lista_lineas=separarLineas(lineas)#lista en la cual cada elemento es una linea diferente
+		
+    	Lista_lineas=compilador_sml.separarLineas(lineas)#lista en la cual cada elemento es una linea diferente
    	for linea in Lista_lineas:
-		print"Dentro de segundo if"
+		
         	if linea[0:3]=="val": #determina si es una variable
-           	 	lista+=separa_variable_valor(linea,"","",4)
-			print"Dentro if linea"
-			print lista
+           	 	lista+=compilador_sml.separa_variable_valor(linea,"","",4)
+			
+			
         	elif linea[0:3]!="fun": #determina si es una funcion
-            		lista+=separa_variable_valor(linea,"","",1)
-			print"Dentro elif linea"
+            		lista+=compilador_sml.separa_variable_valor(linea,"","",1)
+			
         	else:
             		print"Es funcion"
-        print "FIN"	
-   	Lista=verifica(lista) #invoca a la funcion verifica 
-	lista1=tipo_dato(Lista)
-    return render_template('restpla2.html', lista=lista1)
+        	
+   	Lista=compilador_sml.verifica(lista) #invoca a la funcion verifica 
+	Lista=compilador_sml.tipo_dato(Lista)
+	print lista
+    return render_template('restpla2.html', lista=Lista)
 
 @app.route('/Principal')
 def Principal():
